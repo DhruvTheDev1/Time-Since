df0a25c5a861f181a314a0794a9232fdbffc85dc
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2026-01-02 16:10:13 +0000

    // manually testing service class - created timeService instance - tested results e.g. 0 days 0 hours 1 minutes 43 seconds

--- src/main/java/com/dhruvthedev1/time_since/TimeService.java
+++ src/main/java/com/dhruvthedev1/time_since/TimeService.java
@@ -17,13 +17,25 @@ public class TimeService {
 
     Duration duration = Duration.between(selectedInput, currTime); // calculates hours, mins, seconds
 
-    long days = period.getDays();
-    long hours = duration.toHours() % 24;
-    long minutes = duration.toMinutes() % 60;
-    long seconds = duration.toSeconds() % 60;
-
+    // calculates the remaning hours, minutes and seconds
+    // e.g. 58 hours, 130 seconds, 4500 seconds
+    long days = period.getDays(); // gives full days (2)
+    long hours = duration.toHours() % 24; // gives remaning hours after full day (58 % 24 = 10)
+    long minutes = duration.toMinutes() % 60; // gives remaining minutes (130 % 60 = 10)
+    long seconds = duration.toSeconds() % 60; // gives remaning seconds (4500 % 60 = 0)
+    
+    // returns a readable format 2 days, 10 hours, 10 minutes, 0 seconds
     return String.format("%d days %d hours %d minutes %d seconds", days, hours, minutes, seconds);
-
   }
+
+  // testing
+  public static void main(String[] args) {
+    TimeService timeService = new TimeService();    
+    LocalDateTime selectedInput = LocalDateTime.of(2026, 01, 02, 16, 05, 0);
+    String result = timeService.calculateTimeSince(selectedInput);
+    
+    System.out.println(result); 
+  }
+
 
 }
