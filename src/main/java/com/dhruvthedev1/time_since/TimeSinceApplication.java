d5a0639c4fd0d70d948524c03e0c35f08b5f1bfb
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2026-01-02 14:58:06 +0000

    add model

---
+++ src/main/java/com/dhruvthedev1/time_since/TimeSinceApplication.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.time_since;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSinceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSinceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhruvthedev1/time_since/TimeSinceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.time_since;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSinceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
